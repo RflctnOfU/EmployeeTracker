c9b31b6be39f2d1b8d82c04ed72c2db80892100b
Kris Marshall <rflctnofu@hotmail.com>
2022-07-26 20:15:49 -0400

    here it is

--- db/schema.sql
+++ db/schema.sql
@@ -5,7 +5,7 @@ USE tracker_db;
 
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE role (
@@ -22,10 +22,10 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL, 
-    manager_id INT 
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+	FOREIGN KEY (role_id)
+	REFERENCES role(id),
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     -- add keys -- 
